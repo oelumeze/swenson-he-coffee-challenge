067bb28ef37a73789cbb1a740d2b90e51ea65f31
Ogo Elumeze <o.elumeze@yahoo.com>
2019-09-14 13:40:42 -0400

    correction on db connection set up

--- src/connection/index.js
+++ src/connection/index.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
 
-export const connectdb = async() => {
-    console.log("export default connection")
-    
+export const connectdb = async () => {
+
     let mongo_db_url = 'mongodb://localhost:27017/Swenson_Coffee';
-   
-    mongoose.Promise = global.Promise;;
     try {
-        await mongoose.connect(mongo_db_url, { useNewUrlParser: true })
+        await mongoose.connect(mongo_db_url, { useNewUrlParser: true, useUnifiedTopology: true })
     } catch (ex) {
         console.log("error in connetion", ex)
-    } 
+    }
 
     mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as connection from './connection/index';
+import router from './routes/index';
 
 
 const PORT = 4000;
 const app = express();
 app.use(cors());
-app.use(() => {    
-    connection.connectdb();
-});//connection to the database
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+    (req, res, next) => {   
+    connection.connectdb()
+    next();
+}
+);//connection to the database
+app.use('/', router) //set up all routes/endpoints
 
 app.listen(PORT);
 
