fcaf192f8277d5729ad5ce3ab7d2549bb40f402f
Ogo Elumeze <o.elumeze@yahoo.com>
2019-09-14 09:23:14 -0400

    add database connection on server start up

--- src/connection/index.js
+++ src/connection/index.js
@@ -4,14 +4,13 @@ export const connectdb = async() => {
     console.log("export default connection")
     
     let mongo_db_url = 'mongodb://localhost:27017/Swenson_Coffee';
-    // mongoose.connect(mongo_db_url, { useNewUrlParser: true });
-
+   
     mongoose.Promise = global.Promise;;
     try {
         await mongoose.connect(mongo_db_url, { useNewUrlParser: true })
     } catch (ex) {
         console.log("error in connetion", ex)
     } 
-    // const db = mongoose.connection;
+
     mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import * as connection from './connection/index';
+
 
 const PORT = 4000;
 const app = express();
-
 app.use(cors());
+app.use(() => {    
+    connection.connectdb();
+});//connection to the database
+
 app.listen(PORT);
 
 console.log(`server is listening on ${PORT}`);
