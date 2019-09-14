cf7f9d13a2363c2a52e80d810627eed202baf166
Ogo Elumeze <o.elumeze@yahoo.com>
2019-09-14 10:18:07 -0400

    coffee pods and coffee machine schema

---
+++ src/schema/coffeeMachine.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+//coffee machine constants 
+const COFFEE_MACHINE_LARGE = 'COFFEE_MACHINE_LARGE';
+const COFFEE_MACHINE_SMALL = 'COFFEE_MACHINE_SMALL';
+const ESPRESSO_MACHINE = 'ESPRESSO_MACHINE';
+
+
+/**
+ * define enumerations as static properties,
+ * using JS Style Guide for defining enumerations
+ */
+const ProductTypes = Object.freeze({
+    LargeCoffeeMachine: COFFEE_MACHINE_LARGE,
+    SmallCoffeeMachine: COFFEE_MACHINE_SMALL,
+    ExpressoMachine: ESPRESSO_MACHINE
+});
+
+/**
+ * Water Line compatibilities enumerations
+ */
+const WaterLineCompatibilities = Object.freeze({
+    True: true,
+    False: false
+});
+
+const CoffeeMahineSchema = new Schema({
+    itemNumber: {
+        type: String,
+        required: true
+    },
+    product_type: {
+        type: String,
+        enum: Object.values(ProductTypes)
+    },
+    water_line_compatible: {
+        type: Boolean,
+        enum: Object.values(WaterLineCompatibilities)
+    },
+});
+
+/**
+ * assign the entities of the enums as a static object
+ */
+Object.assign(CoffeeMahineSchema.statics, {
+    ProductTypes,
+    WaterLineCompatibilities
+});
+
+export const CoffeeMachineModel = mongoose.model('coffeeMachine', CoffeeMahineSchema);
+
---
+++ src/schema/coffeePods.js
@@ -0,0 +1,84 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+//coffee pods constants 
+const COFFEE_POD_LARGE = 'COFFEE_POD_LARGE';
+const COFFEE_POD_SMALL = 'COFFEE_POD_SMALL';
+const ESPRESSO_POD = 'ESPRESSO_POD';
+
+//coffee flavor constants
+const COFFEE_FLAVOR_VANILLA = 'COFFEE_FLAVOR_VANILLA';
+const COFFEE_FLAVOUR_CARAMEL = 'COFFEE_FLAVOUR_CARAMEL';
+const COFFEE_FLAVOR_PSL = 'COFFEE_FLAVOR_PSL';
+const COFFEE_FLAVOR_MOCHA = 'COFFEE_FLAVOR_MOCHA';
+const COFFEE_FLAVOR_HAZELNUT = 'COFFEE_FLAVOR_HAZELNUT';
+
+//pack size constants
+const ONE_DOZEN = 12;
+const THREE_DOZEN = 36;
+const FIVE_DOZEN = 60;
+const SEVEN_DOZEN = 84;
+
+
+/**
+ * define enumerations as static properties,
+ * using JS Style Guide for defining enumerations
+ */
+const ProductTypes = Object.freeze({
+    LargeCoffeePod: COFFEE_POD_LARGE,
+    SmallCoffeePod: COFFEE_POD_SMALL,
+    ExpressoPod: ESPRESSO_POD
+});
+
+/**
+ * Coffee Flavors enumerations
+ */
+const CoffeeFlavors = Object.freeze({
+   CoffeeFlavorVanilla: COFFEE_FLAVOR_VANILLA,
+   CoffeeFlavorCaramel: COFFEE_FLAVOUR_CARAMEL,
+   CoffeeFlavorPsl: COFFEE_FLAVOR_PSL,
+   CoffeeFlavorMocha: COFFEE_FLAVOR_MOCHA,
+   CoffeeFlavorHazelNut: COFFEE_FLAVOR_HAZELNUT
+});
+
+/**
+ * Pack sizes enumerations
+ */
+const PackSizes = Object.freeze({
+    OneDozen: ONE_DOZEN,
+    ThreeDozen: THREE_DOZEN,
+    FiveDozen: FIVE_DOZEN,
+    SevenDozen: SEVEN_DOZEN
+})
+
+const CoffeePodsSchema = new Schema({
+    itemNumber: {
+        type: String,
+        required: true
+    },
+    product_type: {
+        type: String,
+        enum: Object.values(ProductTypes)
+    },
+    coffee_flavor: {
+        type: String,
+        enum: Object.values(CoffeeFlavors)
+    },
+    pack_size: {
+        type: Number,
+        enum: Object.values(PackSizes)
+    }
+});
+
+/**
+ * assign the entities of the enums as a static object
+ */
+Object.assign(CoffeeMahineSchema.statics, {
+    ProductTypes,
+    CoffeeFlavors,
+    PackSizes
+});
+
+export const CoffeePodsModel = mongoose.model('coffeePods', CoffeePodsSchema);
+
