0fac94568326a786a5ee24015d5822777185bb1c
Ogo Elumeze <o.elumeze@yahoo.com>
2019-09-14 15:01:48 -0400

    create response handler utility for api methods

---
+++ src/utilities/responseHandler.js
@@ -0,0 +1,24 @@
+
+export const success = (res, status) => (entity) => {
+    if(entity) {
+        console.log("entity", entity)
+        let newResponseObject =  { 
+            success: true,
+            error: false,
+            data: entity
+        }
+        res.status(status || 200).json(newResponseObject)
+    }
+    return null;
+}
+
+export const failure = (res, status) => (error) => {
+    if(error) {
+        return res.status(status).json({
+            error: true,
+            message: error.message
+        })
+    }
+    return null;
+   
+}
