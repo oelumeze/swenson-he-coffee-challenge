9968ceedbd8fa3c089ef67279dd96b9c21e11e69
Ogo Elumeze <o.elumeze@yahoo.com>
2019-09-15 00:52:08 -0400

    refractor load scripts to bulk insert coffee machine and coffee pods

--- src/preloaders/index.js
+++ src/preloaders/index.js
@@ -1,27 +1,30 @@
 import dotenv from 'dotenv';
 import { CoffeeMachineModel } from '../schema/coffeeMachine';
 import { CoffeePodsModel } from '../schema/coffeePods';
-import { getCoffeeMachineData } from '../utilities/insertCoffeeMachines';
-import { getCoffeePodsData } from '../utilities/insertCoffeePods';
+import { getCoffeeMachineData } from './insertCoffeeMachines';
+import { getCoffeePodsData } from './insertCoffeePods';
 dotenv.config();
 
-export const checkScriptsLoaded = () => {
-    if (process.env.COFFEE_PODS_SCRIPT_LOADED === "false") {
+export const checkCoffeeMachineDataIsLoaded = () => { 
+    if(process.env.COFFEE_MACHINE_SCRIPT_LOADED === "false") {
         CoffeeMachineModel.insertMany(getCoffeeMachineData())
             .then((coffeeMachine) => {
-                console.log("insert", coffeeMachine)
-            }).catch((error) => {
-                console.log("error", error)
-            })
-    }
-
-    if(process.env.COFFEE_MACHINE_SCRIPT_LOADED === "false") {
-        CoffeePodsModel.insertMany(getCoffeePodsData())
-            .then((coffeePods) => {
 
             }).catch((error) => {
                 console.log("error", error)
             })
     }
     return null;
+}
+
+export const checkCoffeePodDataIsLoaded = () => {
+    if (process.env.COFFEE_PODS_SCRIPT_LOADED === "false") {
+        CoffeePodsModel.insertMany(getCoffeePodsData())
+            .then((coffeePods) => {
+                console.log("insert", coffeePods)
+            }).catch((error) => {
+                console.log("error", error)
+            })
+    }
+    return null
 }
