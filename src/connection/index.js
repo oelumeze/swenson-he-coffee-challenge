413487052bdb85fb03f0034b33ecc5995cf0261d
Ogo Elumeze <o.elumeze@yahoo.com>
2019-10-23 17:42:38 -0500

    prepare package manager for heroku deployment, include atlas mongo url

--- src/connection/index.js
+++ src/connection/index.js
@@ -5,7 +5,11 @@ dotenv.config()
 
 export const connectdb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL+'/'+process.env.MONGODB_DATABASE_NAME, { useNewUrlParser: true, useUnifiedTopology: true })
+        //process.env.MONGODB_URL+'/'+process.env.MONGODB_DATABASE_NAME
+        let remoteConnectionString = 'mongodb+srv://oelumeze:vicente@1988@cluster0-zt76f.mongodb.net/test?retryWrites=true&w=majority';
+        let localConnectionString = process.env.MONGODB_URL+'/'+process.env.MONGO_DATABASE_NAME;
+        let urlConnectionString = process.env.ENVIRONMENT_IS_PRODUCTION === true ? remoteConnectionString : localConnectionString;
+        await mongoose.connect(urlConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     } catch (ex) {
         console.log("error in connetion", ex)
     }
--- src/routes/coffeePods.js
+++ src/routes/coffeePods.js
@@ -5,6 +5,6 @@ const coffeePodsRouter = new Router();
 
 coffeePodsRouter.get('/getByProductType/:product_type', getCoffeePodsByProductType);
 coffeePodsRouter.get('/getByCoffeeFlavor/:coffee_flavor', getCoffeePodsByFlavour);
-coffeePodsRouter.get('/getByPackSizes/:pack_size', getCoffeeByPackSize)
+coffeePodsRouter.get('/getByPackSizes/:pack_size', getCoffeeByPackSize);
 
 export default coffeePodsRouter;
